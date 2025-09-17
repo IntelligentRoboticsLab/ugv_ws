7862e3b210cb1445461bae815bdbd76278b845ec
Arnoud Visser <physar@gmail.com>
2025-09-17 15:29:42 +0200

    Update build_apriltag.sh
    Removed explicit home-directory

--- build_apriltag.sh
+++ build_apriltag.sh
@@ -1,4 +1,4 @@
-cd /home/ws/ugv_ws/src/ugv_else/apriltag_ros/apriltag/
+cd ~/ugv_ws/src/ugv_else/apriltag_ros/apriltag/
 cmake -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --target install
-cd /home/ws/ugv_ws/
+cd ~/ugv_ws/
