26dd536dfa4549aa17d8bd5d96fd70a040fc80a8
Arnoud Visser <physar@gmail.com>
2025-09-17 14:26:32 +0200

    Update build_first.sh
    Make the vizanti-scripts executable

--- build_first.sh
+++ build_first.sh
@@ -1,8 +1,10 @@
 cd ~/ugv_ws
 colcon build --cmake-args -Wno-dev --packages-select cartographer costmap_converter_msgs explore_lite --executor sequential  --symlink-install
 colcon build --cmake-args -Wno-dev --packages-select openslam_gmapping slam_gmapping --executor sequential  --symlink-install
-colcon build --cmake-args -Wno-dev --packages-select rf2o_laser_odometry robot_pose_publisher teb_msgs vizanti vizanti_cpp vizanti_demos vizanti_msgs vizanti_server --executor sequential  --symlink-install
+colcon build --cmake-args -Wno-dev --packages-select rf2o_laser_odometry robot_pose_publisher teb_msgs --executor sequential  --symlink-install
 colcon build --cmake-args -Wno-dev --packages-select ugv_base_node ugv_interface ugv_bringup ugv_chat_ai ugv_description ugv_gazebo ugv_nav ugv_slam ugv_tools ugv_vision ugv_web_app --executor sequential  --symlink-install 
+chmod +x ~/ugv_ws/src/ugv_else/vizanti/vizanti_server/scripts/*.py
+colcon build --cmake-args -Wno-dev --packages-select vizanti vizanti_cpp vizanti_demos vizanti_msgs vizanti_server --executor sequential  --symlink-install
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "source ~/ugv_ws/install/setup.bash" >> ~/.bashrc
 source ~/.bashrc 
