b4e694aab2c6a90a7d544f35da2726066f41076b
Arnoud Visser <physar@gmail.com>
2025-09-17 15:43:54 +0200

    Update build_apriltag.sh
    Build apriltag with colcon build

--- build_apriltag.sh
+++ build_apriltag.sh
@@ -1,4 +1,3 @@
-cd ~/ugv_ws/src/ugv_else/apriltag_ros/apriltag/
-cmake -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build --target install
+cd ~/ugv_ws
+colcon build --cmake-args -Wno-dev --packages-select apriltag apriltag_msgs apriltag_ros --executor sequential  --symlink-install
 cd ~/ugv_ws/
