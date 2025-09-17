baea7fd9543bbc6c37989050d2f000ac3d89c70c
Arnoud Visser <physar@gmail.com>
2025-09-17 15:27:29 +0200

    Update build_common.sh
    Also build ldlidar

--- build_common.sh
+++ build_common.sh
@@ -1,7 +1,7 @@
 cd ~/ugv_ws
 colcon build --cmake-args -Wno-dev --packages-select cartographer costmap_converter_msgs explore_lite --executor sequential  --symlink-install
 colcon build --cmake-args -Wno-dev --packages-select openslam_gmapping slam_gmapping --executor sequential  --symlink-install
-colcon build --cmake-args -Wno-dev --packages-select rf2o_laser_odometry robot_pose_publisher teb_msgs --executor sequential  --symlink-install
+colcon build --cmake-args -Wno-dev --packages-select ldlidar rf2o_laser_odometry robot_pose_publisher teb_msgs --executor sequential  --symlink-install
 colcon build --cmake-args -Wno-dev --packages-select ugv_base_node ugv_interface ugv_bringup ugv_chat_ai ugv_description ugv_gazebo ugv_nav ugv_slam ugv_tools ugv_vision ugv_web_app --executor sequential  --symlink-install
 colcon build --cmake-args -Wno-dev --packages-select vizanti vizanti_cpp vizanti_demos vizanti_msgs vizanti_server --executor sequential  --symlink-install
 source install/setup.bash 
