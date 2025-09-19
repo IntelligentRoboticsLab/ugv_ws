d1a64405340bb33c9c529c3f67a2546341c13f33
Arnoud Visser <physar@gmail.com>
2025-09-19 14:22:17 +0200

    Update install_additional_ros_humble_packages.sh
    silent update

--- install_additional_ros_humble_packages.sh
+++ install_additional_ros_humble_packages.sh
@@ -8,11 +8,11 @@ export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrast
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
-sudo apt install ros-dev-tools
-sudo apt install python3-colcon-clean
-sudo apt install ros-humble-desktop
+sudo apt install ros-dev-tools -y
+sudo apt install python3-colcon-clean -y
+sudo apt install ros-humble-desktop -y
 
 # specific for the UGV Rover
 
