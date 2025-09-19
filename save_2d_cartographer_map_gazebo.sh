fc5172a4d35900d6d0b6ed790923345e1ce19739
Arnoud Visser <physar@gmail.com>
2025-09-19 11:26:45 +0200

    Update save_2d_cartographer_map_gazebo.sh
    removed explicit reference to home

--- save_2d_cartographer_map_gazebo.sh
+++ save_2d_cartographer_map_gazebo.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-cd /home/ws/ugv_ws/src/ugv_main/ugv_gazebo/maps
+#!/bin/bash 
+cd ~/ugv_ws/install/ugv_gazebo/share/ugv_gazebo/maps
 ros2 run nav2_map_server map_saver_cli -f ./map
-ros2 service call /write_state cartographer_ros_msgs/srv/WriteState "{filename: '/home/ws/ugv_ws/src/ugv_main/ugv_gazebo/maps/map.pbstream'}"
+ros2 service call /write_state cartographer_ros_msgs/srv/WriteState "{filename: '~/ugv_ws/install/ugv_gazebo/share/ugv_gazebo/maps/map.pbstream'}"
 cd -
 
