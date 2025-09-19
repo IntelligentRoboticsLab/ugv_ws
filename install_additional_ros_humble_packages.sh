bfed036be631ddb4d6185bc6fcb864488c312ed8
Arnoud Visser <physar@gmail.com>
2025-09-19 14:19:39 +0200

    Update install_additional_ros_humble_packages.sh
    Added origin from instructions

--- install_additional_ros_humble_packages.sh
+++ install_additional_ros_humble_packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# from https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html
 sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 
@@ -13,14 +14,16 @@ sudo apt install ros-dev-tools
 sudo apt install python3-colcon-clean
 sudo apt install ros-humble-desktop
 
-sudo apt-get install ros-humble-nav2-msgs ros-humble-map-msgs
-sudo apt-get install ros-humble-nav2-costmap-2d
-sudo apt-get install ros-humble-rosbridge-suite
-sudo apt-get install ros-humble-nav2-bringup
-sudo apt-get install ros-humble-usb-cam ros-humble-depthai-*
-sudo apt-get install ros-humble-joint-state-publisher-*
-sudo apt-get install ros-humble-robot-localization
-sudo apt-get install ros-humble-imu-tools
-sudo apt-get install ros-humble-cartographer-ros
-sudo apt-get install ros-humble-apriltag ros-humble-apriltag-msgs ros-humble-apriltag-ros
-sudo apt-get install ros-humble-ros-gz
+# specific for the UGV Rover
+
+sudo apt-get install ros-humble-nav2-msgs ros-humble-map-msgs -y
+sudo apt-get install ros-humble-nav2-costmap-2d -y
+sudo apt-get install ros-humble-rosbridge-suite -y
+sudo apt-get install ros-humble-nav2-bringup -y 
+sudo apt-get install ros-humble-usb-cam ros-humble-depthai-* -y
+sudo apt-get install ros-humble-joint-state-publisher-* -y
+sudo apt-get install ros-humble-robot-localization -y
+sudo apt-get install ros-humble-imu-tools -y
+sudo apt-get install ros-humble-cartographer-ros -y
+sudo apt-get install ros-humble-apriltag ros-humble-apriltag-msgs ros-humble-apriltag-ros -y
+sudo apt-get install ros-humble-ros-gz -y
