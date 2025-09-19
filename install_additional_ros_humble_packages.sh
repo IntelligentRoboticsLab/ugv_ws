fcc8680bdcd0b8afc70f357006929199c6f18257
Arnoud Visser <physar@gmail.com>
2025-09-19 14:12:31 +0200

    Update install_additional_ros_humble_packages.sh
    Added ros-apt-source to script

--- install_additional_ros_humble_packages.sh
+++ install_additional_ros_humble_packages.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo
+$VERSION_CODENAME)_all.deb"
+sudo dpkg -i /tmp/ros2-apt-source.deb
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install ros-dev-tools
+sudo apt install python3-colcon-clean
+sudo apt install ros-humble-desktop
+
 sudo apt-get install ros-humble-nav2-msgs ros-humble-map-msgs
 sudo apt-get install ros-humble-nav2-costmap-2d
 sudo apt-get install ros-humble-rosbridge-suite
