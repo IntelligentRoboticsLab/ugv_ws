efdafbc0f09ae3f82eeecffdc4ac45846cba4ee2
Arnoud Visser <physar@gmail.com>
2025-09-19 14:00:52 +0200

    Create install_additional_ros_humble_packages.sh
    Script to install the required ros-humble packages need to build ugv-ws packages

---
+++ install_additional_ros_humble_packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get install ros-humble-nav2-msgs ros-humble-map-msgs
+sudo apt-get install ros-humble-nav2-costmap-2d
+sudo apt-get install ros-humble-rosbridge-suite
+sudo apt-get install ros-humble-nav2-bringup
+sudo apt-get install ros-humble-usb-cam ros-humble-depthai-*
+sudo apt-get install ros-humble-joint-state-publisher-*
+sudo apt-get install ros-humble-robot-localization
+sudo apt-get install ros-humble-imu-tools
+sudo apt-get install ros-humble-cartographer-ros
+sudo apt-get install ros-humble-apriltag ros-humble-apriltag-msgs ros-humble-apriltag-ros
+sudo apt-get install ros-humble-ros-gz
