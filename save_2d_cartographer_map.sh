7da74e35075b9a47406db665e3f04740dcdefe6e
Arnoud Visser <physar@gmail.com>
2025-09-19 11:10:16 +0200

    Update save_2d_cartographer_map.sh
    Removed explicit reference to location of map

--- save_2d_cartographer_map.sh
+++ save_2d_cartographer_map.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/ws/ugv_ws/src/ugv_main/ugv_nav/maps
+cd ~/ugv_ws/install/ugv_nav/share/ugv_nav/maps
 ros2 run nav2_map_server map_saver_cli -f ./map
-ros2 service call /write_state cartographer_ros_msgs/srv/WriteState "{filename: '/home/ws/ugv_ws/src/ugv_main/ugv_nav/maps/map.pbstream'}"
-cd -
+ros2 service call /write_state cartographer_ros_msgs/srv/WriteState "{filename: '~/ugv_ws/install/ugv_nav/share/ugv_nav/maps/map.pbstream'}"
+cd 
 
