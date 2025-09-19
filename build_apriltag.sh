6009cf7a5653142b8018a2c1be56629df46e1149
Arnoud Visser <physar@gmail.com>
2025-09-19 11:04:42 +0200

    Update build_apriltag.sh
    Try again purely with a colcon build

--- build_apriltag.sh
+++ build_apriltag.sh
@@ -1,6 +1,6 @@
 cd ~/ugv_ws
-cd src/ugv_else/apriltag_ros/apriltag/
-cmake -B build -DCMAKE_BUILD_TYPE=Release
-sudo cmake --build build --target install
+# cd src/ugv_else/apriltag_ros/apriltag/
+# cmake -B build -DCMAKE_BUILD_TYPE=Release
+# sudo cmake --build build --target install
 cd ~/ugv_ws
-colcon build --cmake-args -Wno-dev --packages-select apriltag_msgs apriltag_ros --executor sequential  --symlink-install
+colcon build --cmake-args -Wno-dev --packages-select apriltag apriltag_msgs apriltag_ros --executor sequential  --symlink-install
