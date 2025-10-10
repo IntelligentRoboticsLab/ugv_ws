7f907def5b484160c715fc5d2bbe83589311056a
Arnoud Visser <physar@gmail.com>
2025-10-10 15:38:29 +0200

    Update build_apriltag.sh
    --symlink gives clashes between jetson and root

--- build_apriltag.sh
+++ build_apriltag.sh
@@ -1,7 +1,10 @@
 cd ~/ugv_ws
 sudo apt install libopencv-dev=4.5.4+dfsg-9ubuntu4 -y
+
 # cd src/ugv_else/apriltag_ros/apriltag/
 # cmake -B build -DCMAKE_BUILD_TYPE=Release
 # sudo cmake --build build --target install
+
 cd ~/ugv_ws
-colcon build --cmake-args -Wno-dev --packages-select apriltag apriltag_msgs apriltag_ros --executor sequential  --symlink-install
+colcon clean packages --packages-select apriltag apriltag_msgs apriltag_ros -y
+colcon build --cmake-args -Wno-dev --packages-select apriltag apriltag_msgs apriltag_ros --executor sequential 
